dd17b6c1bd04003b899fc51b8a6f158656ed0890
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-12-15 10:04:55 -0500

    feat: improve the description of the problems

--- src/oddEvenPrime.ts
+++ src/oddEvenPrime.ts
@@ -1,10 +1,9 @@
 /**
 The OddEvenPrime Kata
-
 Write a program that prints numbers within specified range lets say 1 to 100.
-If number is odd print 'Odd' instead of the number.
-If number is even print 'Even' instead of number.
-Print number as a string, if it does not meet above two conditions, means if number is Prime.
+- If number is odd print 'Odd' instead of the number.
+- If number is even print 'Even' instead of number.
+- Print number as a string, if number is Prime.
  */
 
 export const game = (input: number) => input;
--- src/stringSum.ts
+++ src/stringSum.ts
@@ -2,6 +2,8 @@
 Write a simple String Sum utility with a function string Sum(string num1, string num2),
 which can accept only natural numbers and will return their sum.
 Replace entered number with 0 (zero) if entered number is not a natural number.
+
+Extra: accept more string numbers to get the addiction
  */
 
 const sum = (numOne: string, num2: string) => '';
