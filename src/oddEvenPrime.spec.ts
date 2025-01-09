a29229948ab79b8f36c3b4b74a2f658f63d2ed9b
nava2105 <150807583+nava2105@users.noreply.github.com>
2025-01-09 09:41:57 -0500

    feat(🛠️ Complete oddEvenPrime): Make the code and tests for oddEvenPrime

--- src/oddEvenPrime.spec.ts
+++ src/oddEvenPrime.spec.ts
@@ -1,9 +1,26 @@
 import { game } from "./oddEvenPrime";
 
 describe('Odd Even Prime code kata', () => {
-  test('should works', () => {
-    const value = 1;
-    const resp = game(value);
-    expect(resp).toEqual(value);
+  test('If the input is -6 the result should be Error', () => {
+    const resp = game(-6);
+    expect(resp).toEqual('Error');
+  });test('If the input is 0 the result should be Error', () => {
+    const resp = game(0);
+    expect(resp).toEqual('Error');
+  });test('If the input is 1 the result should be Odd', () => {
+    const resp = game(1);
+    expect(resp).toEqual('Odd');
+  });
+  test('If the input is 5 the result should be Odd23Even5', () => {
+    const resp = game(5);
+    expect(resp).toEqual('Odd23Even5');
+  });
+  test('If the input is 7 the result should be Odd23Even5Even7', () => {
+    const resp = game(7);
+    expect(resp).toEqual('Odd23Even5Even7');
+  });
+  test('If the input is 13 the result should be Odd23Even5Even7EvenOddEven11Even13', () => {
+    const resp = game(13);
+    expect(resp).toEqual('Odd23Even5Even7EvenOddEven11Even13');
   });
 });
--- src/oddEvenPrime.ts
+++ src/oddEvenPrime.ts
@@ -6,4 +6,20 @@ Write a program that prints numbers within specified range lets say 1 to 100.
 - Print number as a string, if number is Prime.
  */
 
-export const game = (input: number) => input;
+export const game = (input: number) => {
+  if (input<1) return 'Error';
+  const isPrime = (num: number): boolean => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+  let result = "";
+  for (let i = 1; i <= input; i++) {
+    if (isPrime(i)) result += i;
+    else if (i % 2 === 0) result += 'Even';
+    else result += 'Odd';
+  }
+  return result;
+}
